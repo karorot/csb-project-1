906a3a65fccd74c4e4adf480cb8e89ac9776e9fd
karorot <174889933+karorot@users.noreply.github.com>
2026-08-18 20:42:03 +0300

    Add initial templates

--- deardiary/urls.py
+++ deardiary/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.homePageView, name="home"),
-    path("<int:user_id>/", views.diaryView, name="diary"),
-    path("<int:user_id>/<int:entry_id>/", views.entryReadView, name="entry"),
-    path("<int:user_id>/new/", views.entryCreateView, name="new")
+    path("entry/<int:entry_id>/", views.entryReadView, name="entry"),
+    path("entry/new/", views.entryCreateView, name="new")
 ]
--- deardiary/views.py
+++ deardiary/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 
+from .models import Entry
 
-def diaryView(request, user_id):
-    response = "Diary for user %s"
-    return HttpResponse(response % user_id)
 
 def entryReadView(request, entry_id):
-    response = "Entry %s"
-    return HttpResponse(response % entry_id)
+    try:
+        entry = Entry.objects.get(pk=entry_id)
+    except Entry.DoesNotExist:
+        raise Http404("No such entry in the diary")
+    return render(request, 'deardiary/entry.html', {'entry': entry})
 
 def entryCreateView(request):
     response = "Write a new diary entry"
     return HttpResponse(response)
 
 def homePageView(request):
-    return HttpResponse("Hello, world. This is index.")
+    #entries = Entry.objects.filter(writer=request.user)
+
+    entries = ["moi"]
+    context = {'entries' : entries}
+    return render(request, 'deardiary/index.html', context)
