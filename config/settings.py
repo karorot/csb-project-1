a4608d1622da9371e0b1b24a8af985e49ebbb317
karorot <174889933+karorot@users.noreply.github.com>
2026-08-22 21:32:29 +0300

    Add functionality for creating new users

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'deardiary.apps.DeardiaryConfig'
+    'deardiary.apps.DeardiaryConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
