3381dc078317e33e2690de9eb6b054b5b2b728c5
karorot <174889933+karorot@users.noreply.github.com>
2026-08-27 19:53:19 +0300

    Show list of random public entries on index page

--- deardiary/views.py
+++ deardiary/views.py
@@ -4,13 +4,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
 
-from random import choice
+from random import sample
 
 from .models import Entry
 
-def random_entry():
-    public_entries = Entry.objects.filter(public=True)
-    return choice(public_entries)
+def random_entries():
+    public_entries = Entry.objects.filter(public=True).order_by('?')[:5]
+    return list(public_entries)
 
 
 @login_required
@@ -18,7 +18,7 @@ def read_entry(request, entry_id):
     try:
         entry = Entry.objects.get(pk=entry_id)
     except Entry.DoesNotExist:
-        raise Http404("No such entry")
+        raise Http404()
 
     # fix for flaw 1: broken access control
     if request.user != entry.writer:
@@ -55,6 +55,6 @@ def index(request):
     if request.user.is_authenticated:
         entries = Entry.objects.filter(writer=request.user)
         context['entries'] = entries
-        context['random'] = random_entry()
+        context['random'] = random_entries()
 
     return render(request, 'deardiary/index.html', context)
