2899d0e67d71aaafa9a7fd3f8a8798c6771f5850
karorot <174889933+karorot@users.noreply.github.com>
2026-08-28 20:04:52 +0300

    Comment out fix for flaw 3 and add screenshots

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib import messages
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Account created! Log in below.")
+        return response
